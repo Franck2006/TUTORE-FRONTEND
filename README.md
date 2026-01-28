99368a92880a387a298ca0d2fa119320ad523387
Franck <amanifranck2005@gmail.com>
2026-01-28 09:10:01 +0100

    adding the sentence in the read me file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # React + TypeScript + Vite
+# franck is the one who add this 
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline"> this is the first commit of Franck </h1>
+    </>
+  )
+}
+
+export default App
